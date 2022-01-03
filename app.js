f7b9e26feb4a39fa6736e42d745e5be416cfbba5
N3998 <n3998@student.jamk.fi>
2022-01-03 11:15:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,12 +17,3 @@ app.use('/', routes);
 
 app.listen(3000);
 
-/*16.6.2019 Matti Leppäkorpi 20h. Tein palvelimen jonne voi lisätä kolmessa eri kaupungissa toimivan 
-Oy Yhtymä Ab:n varastosaldolle uusia asioita ja hakea varastosaldot, sekä poistaa asioita varastosalolta.
-Tämän lisäksi sivustolta näkee kaupunkikohtaisen viimeisimmän lämpötilan.
-Tein aluksi etusivun html-muotoon, mutta muutin sen pugiksi netistä löytyneen kääntäjän avulla. Css-
-tiedoston kanssa meinasi olla ongelmia kunnes sain siihen apua ja lisäsin routes.js tiedostoon 
-router.use('/static', express.static(path.join(__dirname, '..', 'static'))); 
-Taidot loppuivat siihen kun yritin keksiä miten location-tiedon voisi antaa valmiista vaihtoehdoista 
-käyttäjälle <select>-tagilla. Tämän avulla olisin ehkä voinut keksiä konstin myös varastojen sorttaamisen
-kaupungeittain. Nyt tuo kaupunkikohtainen varastosaldo näyttää koko varaston. */
